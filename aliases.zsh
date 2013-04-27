15bfcacb44446f2e4b2819229ed6a71c4eaaf48f
Dominik Porada <dominik.porada@monterail.com>
2013-04-27 15:00:37 +0200

    Add Ruby-related aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,3 +1,8 @@
 # Print the path or change working directory to the top-most Finder window location
 alias pwdf='osascript -e "tell app \"Finder\" to POSIX path of (insertion location as alias)"'
 alias cdf='cd `pwdf`'
+
+# Ruby-related aliases
+alias rb='rbenv'
+alias be='bundle exec'
+alias bi='bundle install'
