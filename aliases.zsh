f3a9d0dbf48e151d566c6bbeafae5e3a8efc5e26
Dominik Porada <dominik.porada@monterail.com>
2013-05-11 17:41:59 +0200

    Allow aliases to be used with `sudo`

--- aliases.zsh
+++ aliases.zsh
@@ -1,3 +1,6 @@
+# Allow aliases to be used with `sudo`
+alias sudo='sudo '
+
 # Print the path or change working directory to the top-most Finder window location
 alias pwdf='osascript -e "tell app \"Finder\" to POSIX path of (insertion location as alias)"'
 alias cdf='cd `pwdf`'
