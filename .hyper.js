38dfd82606c3bb48f63589ab6ac4107abae35c4d
Dominik Porada <dominik@porada.co>
2017-05-14 01:25:43 +0200

    :nail_care:

--- .hyper.js
+++ .hyper.js
@@ -4,7 +4,8 @@ module.exports = {
     fontSize: 15,
     fontFamily: 'SF Mono, monospace',
     hyperStatusLine: {
-      footerTransparent: false,
+      dirtyColor: 'yellow',
+      footerTransparent: false
     },
 
     // Shell
