668b4ff66857a26396f70528dee941ef3dbf8c3d
Dominik Porada <dominik@porada.co>
2017-05-17 14:11:15 +0200

    Set different prompt character depending on user

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -52,7 +52,11 @@ end
 
 function __fish_prompt_caret
   test $argv[1] -ne 0; and set_color $fish_color_error
-  printf "▲ "
+  switch "$USER"
+    case "Dominik"; printf "▲ "
+    case "pi";      printf "π "
+    case "*";       printf "\$ "
+  end
 end
 
 function __fish_prompt_has_status_line
