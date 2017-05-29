a5ed4d50f05bff0eaa2cb5fe2b7502a1732c353d
Dominik Porada <dominik@porada.co>
2017-05-29 12:43:59 +0200

    Include host in tab title when connected via SSH

--- .config/fish/functions/fish_title.fish
+++ .config/fish/functions/fish_title.fish
@@ -1,4 +1,5 @@
 function fish_title
+  __fish_title_host
   __fish_title_pwd
   __fish_title_command
 end
@@ -9,4 +10,8 @@ end
 
 function __fish_title_pwd
   printf "%s" (prompt_pwd)
+end
+
+function __fish_title_host
+  test -n "$SSH_CONNECTION"; and printf "%s@%s: " $USER (hostname -s)
 end
