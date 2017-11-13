61762926736dbbd2673bcd5c268192438f9dc46c
Dominik Porada <dominik@porada.co>
2017-11-13 14:33:23 +0100

    Don’t set custom terminal title

--- .config/fish/functions/fish_title.fish
+++ .config/fish/functions/fish_title.fish
@@ -1,17 +1,3 @@
 function fish_title
-  __fish_title_host
-  __fish_title_pwd
-  __fish_title_command
-end
-
-function __fish_title_command
-  test $_ != "fish"; and printf " · $_"
-end
-
-function __fish_title_pwd
-  printf "%s" (prompt_pwd)
-end
-
-function __fish_title_host
-  test -n "$SSH_CONNECTION"; and printf "%s@%s: " $USER (hostname -s)
+  printf ""
 end
