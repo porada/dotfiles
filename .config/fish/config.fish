cac4040b1f970017f60fe561cde0664ac732e6f9
Dominik Porada <dominik@porada.co>
2019-05-07 17:31:31 +0200

    Contain Ruby gems locally

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,7 +10,12 @@ set -x MANPAGER "less -X"
 
 # Set dotfiles paths
 set -x DOTFILES_PATH "$HOME/.dotfiles"
+set -x DOTFILES_BIN_PATH "$DOTFILES_PATH/bin"
 set -x DOTFILES_FISH_PATH "$DOTFILES_PATH/.config/fish"
+
+# Set Ruby gem paths
+set -x GEM_HOME "$DOTFILES_PATH/.gems"
+set -x GEM_BIN_PATH = "$GEM_HOME/bin"
 
 # Set dotfiles paths
 set -x PATH "/sbin" $PATH
@@ -18,7 +23,10 @@ set -x PATH "/bin" $PATH
 set -x PATH "/usr/sbin" $PATH
 set -x PATH "/usr/bin" $PATH
 set -x PATH "/usr/local/bin" $PATH
-set -x PATH "$DOTFILES_PATH/bin" $PATH
+set -x PATH "/usr/local/sbin" $PATH
+set -x PATH "/usr/local/opt/ruby/bin" $PATH
+set -x PATH "$GEM_BIN_PATH" $PATH
+set -x PATH "$DOTFILES_BIN_PATH" $PATH
 set -x PATH "./node_modules/.bin" $PATH
 set -x PATH "./bin" $PATH
 
