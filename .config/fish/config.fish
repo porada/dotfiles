c7c0a61d07894265fa9ef10958f1045d6e66ff89
Dominik Porada <dominik@porada.co>
2022-01-12 20:17:40 +0100

    Update paths

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,11 +14,11 @@ set -x DOTFILES_BIN_PATH "$DOTFILES_PATH/bin"
 set -x DOTFILES_FISH_PATH "$DOTFILES_PATH/.config/fish"
 
 # Set Node paths
-set -x NODE_BIN_PATH "/usr/local/opt/node@14/bin"
+set -x NODE_BIN_PATH "/opt/homebrew/opt/node@14/bin"
 
 # Set PHP paths
-set -x PHP_BIN_PATH "/usr/local/opt/php@7.4/sbin"
-set -x PHP_BIN_PATH "/usr/local/opt/php@7.4/bin" $PHP_BIN_PATH
+set -x PHP_BIN_PATH "/opt/homebrew/opt/php@7.4/sbin"
+set -x PHP_BIN_PATH "/opt/homebrew/opt/php@7.4/bin" $PHP_BIN_PATH
 
 # Set dotfiles paths
 set -x PATH "/sbin" $PATH
@@ -26,14 +26,11 @@ set -x PATH "/bin" $PATH
 set -x PATH "/usr/sbin" $PATH
 set -x PATH "/usr/bin" $PATH
 set -x PATH "/usr/local/bin" $PATH
-set -x PATH "/usr/local/sbin" $PATH
 set -x PATH "/opt/homebrew/sbin" $PATH
 set -x PATH "/opt/homebrew/bin" $PATH
 set -x PATH "$PHP_BIN_PATH" $PATH
 set -x PATH "$NODE_BIN_PATH" $PATH
 set -x PATH "$DOTFILES_BIN_PATH" $PATH
-set -x PATH "./node_modules/.bin" $PATH
-set -x PATH "./bin" $PATH
 
 # Load dotfiles
 . "$DOTFILES_FISH_PATH/aliases.fish"
