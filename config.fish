5ef1de63803309ac5e7d46b009811e47f300bd4f
Dominik Porada <dominik@porada.co>
2022-01-13 14:58:39 +0100

    Move Fish dotfiles to root directory

---
+++ config.fish
@@ -0,0 +1,40 @@
+# Set locale variables to prevent possible encoding issues
+set -x LANG "en_US"
+set -x LC_ALL "en_US.UTF-8"
+
+# Set the default editor
+set -x EDITOR "vim -c startinsert"
+
+# Don’t clear the screen after quitting a manual page
+set -x MANPAGER "less -X"
+
+# Set dotfiles paths
+set -x DOTFILES_PATH "$HOME/.dotfiles"
+set -x DOTFILES_BIN_PATH "$DOTFILES_PATH/bin"
+set -x DOTFILES_FISH_PATH "$DOTFILES_PATH/.config/fish"
+
+# Set Node paths
+set -x NODE_BIN_PATH "/opt/homebrew/opt/node@14/bin"
+
+# Set PHP paths
+set -x PHP_BIN_PATH "/opt/homebrew/opt/php@7.4/sbin"
+set -x PHP_BIN_PATH "/opt/homebrew/opt/php@7.4/bin" $PHP_BIN_PATH
+
+# Set dotfiles paths
+set -x PATH "/sbin" $PATH
+set -x PATH "/bin" $PATH
+set -x PATH "/usr/sbin" $PATH
+set -x PATH "/usr/bin" $PATH
+set -x PATH "/usr/local/bin" $PATH
+set -x PATH "/opt/homebrew/sbin" $PATH
+set -x PATH "/opt/homebrew/bin" $PATH
+set -x PATH "$PHP_BIN_PATH" $PATH
+set -x PATH "$NODE_BIN_PATH" $PATH
+set -x PATH "$DOTFILES_BIN_PATH" $PATH
+
+# Define prompt colors
+. "$DOTFILES_FISH_PATH/colors.fish"
+
+# Load dotfiles
+. "$DOTFILES_PATH/aliases.fish"
+. "$DOTFILES_PATH/extra.fish"
