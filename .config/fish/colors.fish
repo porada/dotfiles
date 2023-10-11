95d46eac8bb502085d98e14374399686ddbd6b09
Dominik Porada <dominik@porada.co>
2023-10-11 19:20:07 +0200

    Change color scheme

--- .config/fish/colors.fish
+++ .config/fish/colors.fish
@@ -1,25 +1,38 @@
-set -x fish_color_autosuggestion "brblack"
-set -x fish_color_command "green"
-set -x fish_color_comment "brblack"
-set -x fish_color_cwd "blue"
-set -x fish_color_cwd_root "red"
-set -x fish_color_end "brblack"
-set -x fish_color_error "red"
-set -x fish_color_escape "brblack"
-set -x fish_color_history_current "--bold"
-set -x fish_color_host "magenta"
-set -x fish_color_match "--background=brblack"
-set -x fish_color_normal "normal"
-set -x fish_color_operator "cyan"
-set -x fish_color_param "normal"
-set -x fish_color_quote "yellow"
-set -x fish_color_redirection "blue"
-set -x fish_color_search_match "--background=brblack"
-set -x fish_color_selection "black" "--background=white"
-set -x fish_color_user "magenta"
-set -x fish_color_valid_path "--underline"
-
-set -x fish_pager_color_completion "normal"
-set -x fish_pager_color_description "normal"
-set -x fish_pager_color_prefix "--underline"
-set -x fish_pager_color_progress "brblack"
+# Solarized Light
+set -U fish_color_normal normal
+set -U fish_color_command 586e75
+set -U fish_color_quote 839496
+set -U fish_color_redirection 6c71c4
+set -U fish_color_end 268bd2
+set -U fish_color_error dc322f
+set -U fish_color_param 657b83
+set -U fish_color_comment 93a1a1
+set -U fish_color_match --background=brblue
+set -U fish_color_selection white --bold --background=brblack
+set -U fish_color_search_match bryellow --background=white
+set -U fish_color_history_current --bold
+set -U fish_color_operator 00a6b2
+set -U fish_color_escape 00a6b2
+set -U fish_color_cwd green
+set -U fish_color_cwd_root red
+set -U fish_color_valid_path --underline
+set -U fish_color_autosuggestion 93a1a1
+set -U fish_color_user brgreen
+set -U fish_color_host normal
+set -U fish_color_cancel --reverse
+set -U fish_pager_color_prefix cyan --underline
+set -U fish_pager_color_progress brwhite --background=cyan
+set -U fish_pager_color_completion green
+set -U fish_pager_color_description B3A06D
+set -U fish_pager_color_selected_background --background=white
+set -U fish_color_option
+set -U fish_pager_color_background
+set -U fish_pager_color_secondary_background
+set -U fish_pager_color_secondary_prefix
+set -U fish_pager_color_selected_completion
+set -U fish_pager_color_selected_prefix
+set -U fish_pager_color_secondary_completion
+set -U fish_pager_color_selected_description
+set -U fish_color_keyword
+set -U fish_pager_color_secondary_description
+set -U fish_color_host_remote
