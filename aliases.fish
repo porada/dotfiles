27ebca72851d63071e7352714a987e6a15cb155a
Dominik Porada <dominik@porada.co>
2025-10-03 14:50:36 +0200

    Remove unused aliases

--- aliases.fish
+++ aliases.fish
@@ -4,12 +4,8 @@ abbr p "cd ~/Projects"
 # Shorten the frequently used commands
 abbr c "code"
 abbr g "git"
-abbr k "killall"
 abbr o "open"
 abbr v "vim"
 
 # Show hidden files by default when using `ls`
 alias ls="ls -A"
-
-# Recursively remove given files and directories
-abbr rr "rm -rfv"
