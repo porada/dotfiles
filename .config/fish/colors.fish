efcfdd9875e240d494e0cf10d9dd46765f3bef78
Dom Porada <dom@dom.engineering>
2025-12-29 17:51:49 +0100

    Refactor `colors.fish`

--- .config/fish/colors.fish
+++ .config/fish/colors.fish
@@ -1,26 +1,27 @@
-set -U fish_color_normal normal
+set -U fish_color_autosuggestion --dim
+set -U fish_color_cancel --reverse
 set -U fish_color_command green --bold
-set -U fish_color_quote yellow --bold
-set -U fish_color_redirection blue --bold
-set -U fish_color_end brblack
-set -U fish_color_error brred
-set -U fish_color_param normal
 set -U fish_color_comment white
-set -U fish_color_match white --background=brblue
-set -U fish_color_selection --reverse
-set -U fish_color_search_match --reverse
-set -U fish_color_history_current --bold
-set -U fish_color_operator brblack
-set -U fish_color_escape cyan
 set -U fish_color_cwd brblack --bold
 set -U fish_color_cwd_root red
-set -U fish_color_valid_path --underline
-set -U fish_color_autosuggestion --dim
-set -U fish_color_user brgreen
+set -U fish_color_end brblack
+set -U fish_color_error brred
+set -U fish_color_escape cyan
+set -U fish_color_history_current --bold
 set -U fish_color_host normal
-set -U fish_color_cancel --reverse
-set -U fish_pager_color_prefix cyan --underline
-set -U fish_pager_color_progress --reverse
+set -U fish_color_match white --background=brblue
+set -U fish_color_normal normal
+set -U fish_color_operator brblack
+set -U fish_color_param normal
+set -U fish_color_quote yellow --bold
+set -U fish_color_redirection blue --bold
+set -U fish_color_search_match --reverse
+set -U fish_color_selection --reverse
+set -U fish_color_status red
+set -U fish_color_user brgreen
+set -U fish_color_valid_path --underline
 set -U fish_pager_color_completion green
 set -U fish_pager_color_description normal
+set -U fish_pager_color_prefix cyan --underline
+set -U fish_pager_color_progress --reverse
 set -U fish_pager_color_selected_background --background=white
